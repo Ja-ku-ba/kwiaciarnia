a603311906e0e170166a199b7f6a40bec55d63d5
hp <wulfrykpierozek@gmial.com>
2022-11-22 16:05:16 +0100

    added validation

--- kwiaciarnia/forms.py
+++ kwiaciarnia/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, DataRequired, Email, ValidationError
 
 class PostForm(FlaskForm):
     title = StringField(label="Nagłówek: ")
@@ -8,13 +9,13 @@ class PostForm(FlaskForm):
 
 
 class UserForm(FlaskForm):
-    username = StringField(label="Nazwa użytkownika: ")
-    email = StringField(label='Aadres email: ')
-    password1 = PasswordField(label='Hasło: ')
-    password2 = PasswordField(label='Potwierdzenie hasła: ')
+    username = StringField(label="Nazwa użytkownika: ", validators=[Length(min=4, max=32), DataRequired()])
+    email = StringField(label='Aadres email: ', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Hasło: ', validators=[Length(min=8, max=64), DataRequired()])
+    password2 = PasswordField(label='Potwierdzenie hasła: ', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label="Stwórz konto")
 
 class Loginform(FlaskForm):
-    email = StringField(label='Adres email: ')
-    password = PasswordField(label='Hasło: ')
+    email = StringField(label='Adres email: ', validators=[Email()])
+    password = PasswordField(label='Hasło: ', validators=[DataRequired()])
     submit = SubmitField(label="Zaloguj")
